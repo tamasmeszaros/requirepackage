a8a8aca695803be06514692e20b4db5e2d1bcdfe
tamasmeszaros <meszaros.q@gmail.com>
2019-11-23 11:44:45 +0100

    Rewamp package detection and setup script framework

--- deps/PackageSetup.cmake
+++ deps/PackageSetup.cmake
@@ -1,7 +1,5 @@
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../cmake-modules)
-set(DESTDIR "${CMAKE_CURRENT_BINARY_DIR}/destdir" CACHE PATH "Destination directory")
-set(CMAKE_INSTALL_PREFIX "${DESTDIR}/usr/local" CACHE STRING "" FORCE)
-list(APPEND CMAKE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX})
+
+set(CMAKE_INSTALL_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/destdir/usr/local" CACHE STRING "" FORCE)
 
 set(RP_Boost_VERSION 1.70)
 
@@ -9,13 +7,12 @@ if (NOT RP_Boost_COMPONENTS)
     set(RP_Boost_COMPONENTS system iostreams filesystem thread log locale regex)
 endif ()
 
-set(BUILD_SHARED_LIBS OFF CACHE BOOL "")
-set(RP_FORCE_DOWNLOADING ON CACHE BOOL "")
+# set(RP_FORCE_DOWNLOADING ON CACHE BOOL "")
 #set(RP_FIND_QUIETLY ON CACHE BOOL "")
 
-# if(NOT WIN32)
-#     list(REMOVE_ITEM RP_PACKAGES ZLIB)
+#if(NOT WIN32)
+#    list(REMOVE_ITEM RP_PACKAGES ZLIB)
 #     # list(REMOVE_ITEM RP_PACKAGES wxWidgets)
-# endif()
+#endif()
 
 set(RP_ALL_TARGETS ${RP_PACKAGES})
