efe4f2dcd548e4c7100f9f8cb850215bb2e86919
tamasmeszaros <meszaros.q@gmail.com>
2019-11-28 23:27:14 +0100

    Fixes for debug and shared builds

--- cmake-modules/FindOpenCSG.cmake
+++ cmake-modules/FindOpenCSG.cmake
@@ -4,11 +4,6 @@ find_library(OpenCSG_LIBS NAMES opencsg)
 
 find_file(OpenCSG_HEADER NAMES opencsg.h opencsg/opencsg.h)
 
-if (NOT OpenCSG_FIND_QUIETLY)
-    message(STATUS "OpenCSG libs: ${OpenCSG_LIBS}")
-    message(STATUS "OpenCSG include: ${OpenCSG_HEADER}")
-endif()
-
 if (NOT OpenCSG_LIBS OR NOT OpenCSG_HEADER)
     find_dependency(OpenCSG CONFIG)
 else()
@@ -20,4 +15,9 @@ else()
         INTERFACE_INCLUDE_DIRECTORIES "${OpenCSG_INCLUDE_DIR}"
         )
     set(OpenCSG_FOUND TRUE)
+endif()
+
+if (NOT OpenCSG_FIND_QUIETLY)
+    message(STATUS "OpenCSG libs: ${OpenCSG_LIBS}")
+    message(STATUS "OpenCSG include: ${OpenCSG_HEADER}")
 endif()
