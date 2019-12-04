68bd927a9c3f84321100892d43f32c6deccb3524
tamasmeszaros <meszaros.q@gmail.com>
2019-12-04 17:43:10 +0100

    Try to fix finding tbb and big object error with openvdb

--- cmake-modules/overrides/FindOpenEXR.cmake
+++ cmake-modules/overrides/FindOpenEXR.cmake
@@ -1,5 +1,11 @@
 
 include(CMakeFindDependencyMacro)
-find_dependency(IlmBase)
-set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../)
-find_dependency(OpenEXR ${OpenEXR_FIND_VERSION} COMPONENTS ${OpenEXR_FIND_COMPONENTS})
+
+function(openexr_scopeguard)
+    find_dependency(IlmBase)
+    set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../)
+    find_dependency(OpenEXR ${OpenEXR_FIND_VERSION} COMPONENTS ${OpenEXR_FIND_COMPONENTS})
+    set(OpenEXR_FOUND ${OpenEXR_FOUND} PARENT_SCOPE)
+endfunction()
+
+openexr_scopeguard()
