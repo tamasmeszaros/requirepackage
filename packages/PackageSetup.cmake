25886419219688248b637cf2025886934bb97216
tamasmeszaros <meszaros.q@gmail.com>
2024-05-06 23:24:12 +0200

    Fix build

--- packages/PackageSetup.cmake
+++ packages/PackageSetup.cmake
@@ -1,24 +1,6 @@
 set(RP_INSTALL_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/destdir/usr/local" CACHE STRING "")
+set(RP_DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}/downloads CACHE STRING "Dependencies download location")
 
-set(RP_Boost_VERSION 1.70)
-
-if (NOT RP_Boost_COMPONENTS)
-    set(RP_Boost_COMPONENTS system iostreams filesystem thread log locale regex)
-endif ()
-
-set (RP_OpenVDB_COMPONENTS openvdb)
-set (RP_wxWidgets_VERSION 3.1.3)
-
-# set(RP_FORCE_DOWNLOADING ON CACHE BOOL "")
-#set(RP_FIND_QUIETLY ON CACHE BOOL "")
-
-#if(NOT WIN32)
-#    list(REMOVE_ITEM RP_PACKAGES ZLIB)
-#     # list(REMOVE_ITEM RP_PACKAGES wxWidgets)
-#endif()
-
-if (NOT BUILD_SHARED_LIBS)
-    set(TBB_STATIC ON)
-endif ()
+# TODO: set package option defaults
 
 set(RP_ALL_TARGETS ${RP_PACKAGES})
